4453bc40ffb226a3a9ef98d34fbf77365b12a04d
Oliver Steiner <oliver@mollo.ch>
2015-11-16 10:14:48 +0100

    Update vocabulare.js

--- vocabulare.js
+++ vocabulare.js
@@ -9,11 +9,33 @@
   Es geht auch ohne.
 */
 
+// Variablen:
+var wert = "";
 
-// Eine Variable ist ein Stellvertreter für einen beliebigen Wert
-// Variable
+// Eine Variable ist ein Stellvertreter für einen beliebigen Wert.
+// Variablen werden mit einem    var   definiert.
+// Wir weisen der Variable gleich noch einen Wert zu. Dieser ist in 
+// Anführungs- und Schlusszeichen eingeschlossen. 
+// Es können einfache '' oder doppelte "" sein.
+// Alle Texte müssen immer mit Anführungs- und Schlusszeichen sein. Das ist dann ein "string"
 
-var wert = "Hallo Welt";
+// einer Variable wird ein String zugewiesen
+var wert_mit_text = "Hallo Welt";
+
+// Zahlen werden ohne Anführungs- und Schlusszeichen gesetzt.
+var wert_mit_zahl = 123;
+
+// jetzt können wir schon rechenen:
+
+var bananen = 3;
+var birnen = 7;
+
+var fruechte = bananen + birnen;
+print(fruechte);
+
+
+
+
 
 
 
